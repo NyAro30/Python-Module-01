a409c6e1f852f82cec8bd84522ead4f1053ca0c0
Mandimby Ny Aro Rasolonjanahary <mny-aro-@c1r8s14.42antananarivo.mg>
2026-04-21 13:27:36 +0300

    second commit

---
+++ ex2/ft_plant_growth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+class Plant:
+    def __init__(self, name: str, height: float, age: int, growth_rate: float)-> None:
+        self.name = name.capitalize()
+        self.height = height
+        self.age_days = age
+        self.growth_rate = growth_rate
+
+    def show(self)-> None:
+        h = round(self.height, 1)
+        if (self.age == 0 or self.age == 1):
+            print(f"{self.name}: {h}cm, {self.age_days} day old")
+        else:
+            print(f"{self.name}: {h}cm, {self.age_days} days old")
+
+    def age(self)-> int:
+        self.age_days += 1
+
+    def grow(self)-> int:
+        self.height += self.growth_rate
+
+if __name__ == "__main__":
+    rose = Plant("Rose", 25, 30, 0.8)
+    print("=== Garden Plant Growth ===")
+    rose.show()
+
+    starting_height = rose.height
+    for day in range(1, 8):
+        print(f"=== Day {day} ===")
+        rose.grow()
+        rose.age()
+        rose.show()
+    total_growth = round(rose.height - starting_height, 1)
+    print(f"Growth this week: {total_growth}cm")
+    
